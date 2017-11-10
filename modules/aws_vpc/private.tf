166f5863beb670333a7851a43aa9437ad4ece43d
Andrey Larin <andrey.larin@russianpost.ru>
2017-11-10 18:29:26 +0300

    adds routing for private network

--- modules/aws_vpc/private.tf
+++ modules/aws_vpc/private.tf
@@ -15,6 +15,27 @@ resource "aws_eip" "self" {
   vpc = true
 }
 
+resource "aws_route_table" "private" {
+  count  = "${length(var.private_subnets)}"
+  vpc_id = "${aws_vpc.environment.id}"
+
+  tags {
+    Name = "${var.environment}-private"
+  }
+}
+
+resource "aws_route" "private_nat_gateway" {
+  count                  = "${length(var.private_subnets)}"
+  route_table_id         = "${aws_route_table.private.*.id[count.index]}"
+  destination_cidr_block = "0.0.0.0/0"
+  nat_gateway_id         = "${aws_nat_gateway.environment.*.id[count.index]}"
+}
+
+resource "aws_route_table_association" "private" {
+  count          = "${length(var.private_subnets)}"
+  subnet_id      = "${aws_subnet.private.*.id[count.index]}"
+  route_table_id = "${aws_route_table.private.*.id[count.index]}"
+}
 
 # Create private networks
 resource "aws_subnet" "private" {
