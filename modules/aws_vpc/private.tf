2a4c5f5d86c711f6b748aa4075f37804ac1ef458
Andrey Larin <andrey.larin@russianpost.ru>
2017-11-10 18:33:56 +0300

    adds some changes

--- modules/aws_vpc/private.tf
+++ modules/aws_vpc/private.tf
@@ -10,27 +10,27 @@ resource "aws_nat_gateway" "self" {
   subnet_id     = "${aws_subnet.public.*.id[0]}"
 }
 
+# Create elastic IP for nat gateway
 resource "aws_eip" "self" {
   count = "${length(var.private_subnets) - (length(var.private_subnets) -1)}"
   vpc = true
 }
 
+# Create route table for private network
 resource "aws_route_table" "private" {
-  count  = "${length(var.private_subnets)}"
-  vpc_id = "${aws_vpc.environment.id}"
-
-  tags {
-    Name = "${var.environment}-private"
-  }
+  count  = "${length(var.private_subnets) - (length(var.private_subnets) -1)}"
+  vpc_id = "${aws_vpc.self.id}"
 }
 
-resource "aws_route" "private_nat_gateway" {
+# Create route for private network
+resource "aws_route" "through_nat_gateway" {
   count                  = "${length(var.private_subnets)}"
   route_table_id         = "${aws_route_table.private.*.id[count.index]}"
   destination_cidr_block = "0.0.0.0/0"
-  nat_gateway_id         = "${aws_nat_gateway.environment.*.id[count.index]}"
+  nat_gateway_id         = "${aws_nat_gateway.self.*.id[count.index]}"
 }
 
+# create association with route
 resource "aws_route_table_association" "private" {
   count          = "${length(var.private_subnets)}"
   subnet_id      = "${aws_subnet.private.*.id[count.index]}"
