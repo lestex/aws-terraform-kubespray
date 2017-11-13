41a1b9030694b1f1c0c52849e04fb5ce8e890570
Andrey Larin <andrey.larin@russianpost.ru>
2017-11-13 11:38:00 +0300

    adds outputs to aws_vpc module

---
+++ modules/aws_vpc/outputs.tf
@@ -0,0 +1,18 @@
+######################################		
+# output variables		
+######################################		
+output "vpc_id" {
+  value = "${aws_vpc.self.id}"
+}
+
+output "public_subnets" {
+  value = "${aws_subnet.public.*.id}"
+}
+
+output "default_sg" { 
+  value = "${aws_vpc.self.default_security_group_id}"
+}
+
+output "allow_ssh-sg" { 
+  value = "${aws_security_group.allow_ssh-sg.id}"
+}
