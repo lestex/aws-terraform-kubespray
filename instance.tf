95e6b59c15d9fcb1a64505254752ff3166a97a35
Andrey Larin <andrey.larin@russianpost.ru>
2017-11-13 16:24:59 +0300

    some changes

--- instance.tf
+++ instance.tf
@@ -1,16 +1,32 @@
-resource "aws_instance" "instance" {
-  count         = "${length(var.public_subnets)}"
+resource "aws_instance" "instance-public" {
   ami           = "ami-1e339e71"
   instance_type = "t2.nano"
 
   vpc_security_group_ids = ["${module.vpc.default_sg}", "${module.vpc.allow_ssh-sg}"]
-  subnet_id              = "${module.vpc.public_subnets[count.index]}"
+  subnet_id              = "${module.vpc.public_subnets[0]}"
   key_name               = "mykeypair"
 
   tags {
     Name              = "development"
     builtWith         = "terraform"
-    KubernetesCluster = "${var.name}"
+    KubernetesCluster = "k8s"
+  }
+
+  depends_on = ["module.vpc"]
+}
+
+resource "aws_instance" "instance-private" {
+  ami           = "ami-1e339e71"
+  instance_type = "t2.nano"
+
+  vpc_security_group_ids = ["${module.vpc.allow_ssh-sg}"]
+  subnet_id              = "${module.vpc.private_subnets[0]}"
+  key_name               = "mykeypair"
+
+  tags {
+    Name              = "development"
+    builtWith         = "terraform"
+    KubernetesCluster = "k8s"
   }
 
   depends_on = ["module.vpc"]
@@ -22,5 +38,5 @@ resource "aws_key_pair" "mykeypair" {
 }
 
 output "instances" {
-  value = "${aws_instance.instance.*.public_ip}"
+  value = "${aws_instance.instance-public.public_ip}"
 }
--- vpc.tf
+++ vpc.tf
@@ -3,10 +3,12 @@ module "vpc" {
 
   environment             = "development"
   region                  = "${var.region}"
-  vpc_cidr                = "${var.vpc_cidr}"
-  name                    = "${var.name}"
+  vpc_cidr                = "10.0.0.0/16"
+  name                    = "aws-kubernetes"
   map_public_ip_on_launch = true
-  public_subnets          = "${var.public_subnets}"
-  private_subnets         = "${var.private_subnets}"
-  multi_nat_gateway       = true
+  public_subnets          = ["10.0.1.0/24", "10.0.2.0/24"]
+  private_subnets         = ["10.0.10.0/24", "10.0.20.0/24"]
+  enable_nat_gateway      = true
+
+  #multi_nat_gateway       = true
 }
